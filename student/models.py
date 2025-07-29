1d208103865e154b9ca3622824c161cc539eb730
Manojbl <manojshaiva555@gmail.com>
2025-07-29 21:21:37 +0530

    Done

--- student/models.py
+++ student/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name=models.CharField(max_length=100)
-    email=models.CharField()
+    email=models.CharField(max_length=100)
     roll_no=models.CharField(max_length=20)
     course=models.CharField(max_length=100)
 
